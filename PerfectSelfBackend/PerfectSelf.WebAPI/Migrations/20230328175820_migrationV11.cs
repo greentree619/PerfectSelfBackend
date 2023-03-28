413a8f791b855fff7d4c5170e4abdb766887c245
Green Full Stack <greentree619@outlook.com>
2023-03-28 20:03:51 +0200

    Add book room uid

---
+++ PerfectSelfBackend/PerfectSelf.WebAPI/Migrations/20230328175820_migrationV11.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PerfectSelf.WebAPI.Migrations
+{
+    public partial class migrationV11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "ReaderProfile",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Skills",
+                table: "ReaderProfile",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "HourlyPrice",
+                table: "ReaderProfile",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "About",
+                table: "ReaderProfile",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RoomUid",
+                table: "Book",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomUid",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "ReaderProfile",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Skills",
+                table: "ReaderProfile",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "HourlyPrice",
+                table: "ReaderProfile",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "About",
+                table: "ReaderProfile",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- PerfectSelfBackend/PerfectSelf.WebAPI/Models/Book.cs
+++ PerfectSelfBackend/PerfectSelf.WebAPI/Models/Book.cs
@@ -8,6 +8,7 @@
     {
         public Guid ActorUid { get; set; }
         public Guid ReaderUid { get; set; }
+        public Guid RoomUid { get; set; } = Guid.NewGuid();
         public DateTime BookStartTime { get; set; }
         public DateTime BookEndTime { get; set; }
         public String ScriptFile { get; set; }
