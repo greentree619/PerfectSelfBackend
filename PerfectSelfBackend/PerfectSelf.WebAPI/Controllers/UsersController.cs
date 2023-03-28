9077477f13df0df8f153f8dbaeafb1dc8826594b
tonybingo-stack <tonybingo19970425@outlook.com>
2023-03-29 01:17:15 +1100

    PUT user api updated

--- PerfectSelfBackend/PerfectSelf.WebAPI/Controllers/UsersController.cs
+++ PerfectSelfBackend/PerfectSelf.WebAPI/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using PerfectSelf.WebAPI.Common;
 using PerfectSelf.WebAPI.Context;
 using PerfectSelf.WebAPI.Models;
+using static PerfectSelf.WebAPI.Models.User;
 
 namespace PerfectSelf.WebAPI.Controllers
 {
@@ -62,33 +63,20 @@ namespace PerfectSelf.WebAPI.Controllers
             {
                 return NotFound();
             }
+            //user.Uid = u.Uid;
+            if(user.UserName.Length != 0) u.UserName = user.UserName;
+            if (user.Password.Length != 0) u.Password = user.Password;
+            if (user.FirstName.Length != 0) u.FirstName = user.FirstName;
+            if (user.LastName.Length != 0) u.LastName = user.LastName;
+            if (user.Gender >= 0) u.Gender = user.Gender;
+            if (user.CurrentAddress.Length != 0) u.CurrentAddress = user.CurrentAddress;
+            if (user.PermanentAddress.Length != 0) u.PermanentAddress = user.PermanentAddress;
+            if (user.City.Length != 0) u.City = user.City;
+            if (user.Nationality.Length != 0) u.Nationality = user.Nationality;
+            if (user.PhoneNumber.Length != 0) u.PhoneNumber = user.PhoneNumber;
+            if (user.UserType != AccountType.Nothing) u.UserType = user.UserType;
 
-            //_context.Entry(user).State = EntityState.Modified;
-            _context.Entry(u).CurrentValues.SetValues(user);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                throw;
-            }
-
-            return NoContent();
-        }
-        // PUT: api/Users/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("name/{uid}")]
-        public async Task<IActionResult> PutUser(string uid, string name)
-        {
-            var u = await _context.Users.FirstOrDefaultAsync(p => p.Uid.ToString() == uid);
-            if (u == null)
-            {
-                return NotFound();
-            }
-            u.UserName = name;
             _context.Entry(u).State = EntityState.Modified;
-
             try
             {
                 await _context.SaveChangesAsync();
@@ -100,6 +88,30 @@ namespace PerfectSelf.WebAPI.Controllers
 
             return NoContent();
         }
+        //// PUT: api/Users/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("name/{uid}")]
+        //public async Task<IActionResult> PutUserName(string uid, User user)
+        //{
+        //    var u = await _context.Users.FirstOrDefaultAsync(p => p.Uid.ToString() == uid);
+        //    if (u == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    u.UserName = user.UserName;
+        //    _context.Entry(u).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        throw;
+        //    }
+
+        //    return NoContent();
+        //}
         // POST: api/Users
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
